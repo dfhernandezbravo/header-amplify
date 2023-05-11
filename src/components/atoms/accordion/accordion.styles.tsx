e9d70b293c3da143756f75a19f0526446783de2d
Pablo Clemente <martin.clemente@cencosud.com>
2023-05-11 17:10:56 -0300

    Fix in accordion icons

--- src/components/atoms/accordion/accordion.styles.tsx
+++ src/components/atoms/accordion/accordion.styles.tsx
@@ -20,22 +20,30 @@ export const Title = styled.div`
     cursor: pointer;
     border-bottom: 1px solid #666;
     margin-bottom: 1.5rem;
-
     
     p{
         margin-bottom: 1.5rem;
     }
+
+    span{
+        font-weight: 200;
+        font-size: 2rem;
+
+        &[data-color="red"]{
+            color: red;
+        }
+    }
 `;
 
-export const IconContainer = styled.div`
-    height: 16px;
-    width: 16px;
-    display: flex;
-    align-items: start;
-    align-content: flex-start;
-    font-size: 20px;
-    color: red;
-`;
+// export const IconContainer = styled.div`
+//     height: 16px;
+//     width: 16px;
+//     display: flex;
+//     align-items: start;
+//     align-content: flex-start;
+//     font-size: 20px;
+//     color: red;
+// `;
 
 export const Content:any = styled.div`
     padding: 0 0 1rem;
