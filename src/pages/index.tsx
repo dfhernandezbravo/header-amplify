c1060515e9300c8d003ed19eef3f5306b8f29d5e
Carlos Pitre <carlospitre@Carloss-Mac-mini.local>
2023-05-31 16:04:32 -0500

    refactor: add paths in ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import { Provider } from 'react-redux';
 
 import Head from 'next/head';
 
-import Footer from '@components/layouts/footer';
+import Footer from '@modules/footer';
+import Header from '@modules/header';
 import store from '@store/index';
-import Header from '@components/layouts/header';
 
 export default function Home() {
   return (
