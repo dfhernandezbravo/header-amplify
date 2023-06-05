3766f322271c815b176950138e414c91de7c9702
Carlos Pitre <carlospitre@Carloss-Mac-mini.local>
2023-06-05 12:30:04 -0500

    fix: Add provider to Header

--- src/presentation/modules/header/index.tsx
+++ src/presentation/modules/header/index.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable react-hooks/rules-of-hooks */
+import { Provider } from 'react-redux';
 import HeaderDesktop from './layouts/header-desktop';
 import HeaderMobile from './layouts/header-mobile';
+import store from '@store/index';
 
 const Header = () => {
   return (
-    <>
+    <Provider store={store}>
       <HeaderMobile />
       <HeaderDesktop />
-    </>
+    </Provider>
   );
 };
 export default Header;
