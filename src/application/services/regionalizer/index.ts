c4c112ec923e1786c95717d3bcf692b7ca456b14
Jose Martinez <josemanuel.martinez@cencosud.cl>
2023-10-10 16:44:04 -0300

    fix: change endpoint regionalizer

--- src/application/services/regionalizer/index.ts
+++ src/application/services/regionalizer/index.ts
@@ -1,8 +1,8 @@
-import cmsInstance from '@data-sources/cms-instance';
 import RegionalizerService from '@interfaces/regionalizer-service.interface';
+import { bffWebInstance } from '../../data-sources/bbf-web-instance';
 
 const regionalizerService: RegionalizerService = {
-  getRegions: () => cmsInstance.get('/Shipping/regionComunas'),
+  getRegions: () => bffWebInstance.get('/cms/group/Shipping/regionComunas'),
 };
 
 export default regionalizerService;
