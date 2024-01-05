cc253d193c0c91889dee00cc9a4dc1b7d4333034
guillermoferrazcencosud <guillermo.ferrazcuello@cencosud.com>
2024-01-05 00:04:51 -0300

    feat: return only four products suggestions

--- src/domain/use-cases/search/get-products-suggestions.ts
+++ src/domain/use-cases/search/get-products-suggestions.ts
@@ -7,7 +7,10 @@ export const getProductsSuggestions = createAsyncThunk(
   async (params: ProductSuggestionsRequest) => {
     try {
       const { data } = await searchService.getProductSuggestions(params);
-      return data.products;
+      const limit = 4;
+      const length = data?.products?.length || 0;
+      if (length === 0) return [];
+      else return length > 4 ? data?.products?.slice(0, limit) : data?.products;
     } catch (error) {
       throw new Error('Oh no!!');
     }
