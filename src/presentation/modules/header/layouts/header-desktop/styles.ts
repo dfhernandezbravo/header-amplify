f293068482a905a9f2138c2de94a768d05097750
Carlos Pitre <julio.pitremartinez@externos-cl.cencosud.com>
2024-01-17 16:10:23 -0500

    fix: padding right in header

--- src/presentation/modules/header/layouts/header-desktop/styles.ts
+++ src/presentation/modules/header/layouts/header-desktop/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const HeaderDesktopContainer = styled.div`
   display: flex;
-  width: 100%;
+  /* width: 100%; */
   flex-direction: column;
   align-items: center;
 `;
@@ -10,9 +10,11 @@ export const HeaderDesktopContainer = styled.div`
 export const FirstRow = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   gap: 15px;
   margin: 0.5rem 0.5rem 0 0.5rem;
   width: 98%;
+  padding: 0 10px 0 0;
 `;
 
 export const SecondRow = styled.div`
@@ -20,13 +22,13 @@ export const SecondRow = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 98%;
+  padding: 0 10px 0 0;
   margin-bottom: 10px;
 `;
 
 export const HeaderDesktopSearchSection = styled.div`
   display: flex;
   flex: 1;
-  max-width: 1250px;
 `;
 
 export const Divider = styled.div`
