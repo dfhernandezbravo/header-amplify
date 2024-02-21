ef5516f9780df7b89fa164c2c258c97ee9a33578
guillermoferrazcencosud <guillermo.ferrazcuello@cencosud.com>
2024-02-21 13:02:51 -0300

    fix: condition to get categories only in environment staging and no hybrid mode

--- src/domain/use-cases/category/get-categories.ts
+++ src/domain/use-cases/category/get-categories.ts
@@ -2,8 +2,9 @@ import categoryService from '@services/category';
 
 const getCategories = async () => {
   try {
+    const env = process.env.NEXT_PUBLIC_ENV;
     const vtxorderform = localStorage.getItem('vtxorderform');
-    if (vtxorderform) return [];
+    if (vtxorderform && env === 'PRODUCTION') return [];
     const { data } = await categoryService.getCategories();
     return data;
   } catch (error) {
