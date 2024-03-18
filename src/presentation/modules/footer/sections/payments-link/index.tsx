2f2aa59d183a05b41276cdc000c0690d1a763205
pbourlot <paula.bourlot@externos-ar.cencosud.com>
2024-03-17 21:33:44 -0300

    fix: footer links

--- src/presentation/modules/footer/sections/payments-link/index.tsx
+++ src/presentation/modules/footer/sections/payments-link/index.tsx
@@ -1,26 +1,31 @@
+import { useDevice } from '@cencosud-ds/easy-design-system';
 import { FooterPaymentsLinks } from '@entities/footer/footer.entity';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Container, Links } from './style';
 
 interface Props {
   data: FooterPaymentsLinks[];
 }
 
 const PaymentsLinkFooter = ({ data }: Props) => {
+  const { device } = useDevice();
   return (
-    <div>
+    <Container mobile={device !== 'Desktop'}>
       <h4>Medios de pago</h4>
-      {data.map((payment) => (
-        <Link href="" key={payment.title}>
-          <Image
-            src={payment.icon}
-            alt={payment.title}
-            width={40}
-            height={40}
-          />
-        </Link>
-      ))}
-    </div>
+      <Links>
+        {data.map((payment) => (
+          <Link href="" key={payment.title}>
+            <Image
+              src={payment.icon}
+              alt={payment.title}
+              width={40}
+              height={40}
+            />
+          </Link>
+        ))}
+      </Links>
+    </Container>
   );
 };
 
